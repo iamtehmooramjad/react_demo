7bc6f97dc7937cfb1316d0e104fc6452545b8fce
Tehmoor Amjad <AR.Developer@accessretailpk.local>
2022-08-26 16:18:58 +0500

    Use State to fetch data from api and then render on ui on each click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
-import Form from "./components/Form"
-import SignupForm from "./components/SignupForm"
+
 
 export default function App() {
+
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(1)
+
+    console.log("Component rendered")
+
+    /**
+     * Side effects:
+     * If we don't provide second param to the useEffect Hook, it will repeatedly call this function after rendering ui components is sequence.
+     */
+
+    React.useEffect(function() {
+        console.log("Effect ran")
+        fetch(`https://swapi.dev/api/people/${count}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+    }, [count])
+
     return (
         <div>
-            <SignupForm />
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     )
 }
